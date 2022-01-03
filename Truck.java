1cc7f681d003d1932ffd55cf86cdfaf423ead4ee
Myles-Trump <82889818+Myles-Trump@users.noreply.github.com>
2022-01-03 15:07:53 -0500

    Create Truck.java

---
+++ Truck.java
@@ -0,0 +1,26 @@
+/*
+* The "Truck" class
+*
+* @author Myles Trump
+* @version 1.0
+* @since 2022-01-01
+*/
+
+public class Truck extends Vehicle {
+
+    /**
+    * Calling methods from its superclass (Vehicle).
+    */
+    public Truck() {
+        super();
+    }
+
+    // Methods
+    /**
+    * The provideAir() method changes the rpm of the bike.
+    */
+    public void provideAir() {
+        System.out.println("\nYou have recycled your air!");
+    }
+
+}
